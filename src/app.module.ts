f5ae3e522fd0811e6f76429b3dc21ef0850dbb0f
Zhurvlad <murmanskvmf@mail.ru>
2022-11-02 01:45:01 +0300

    Создал первую таблицу User и подключил всё к БД

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule} from "@nestjs/config";
+
+@Module({
+  imports: [UserModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // This for development
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
